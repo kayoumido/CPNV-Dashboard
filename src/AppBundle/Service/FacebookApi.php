01d8fceb8c16a6924e551dcc7c9030dfb4c58438
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-03-09 11:50:20 +0100

    implement FacebookApi service
    The service will use facebooks PHP sdk to get the cpnvs nb of likes and X last posts

--- src/AppBundle/Service/FacebookApi.php
+++ src/AppBundle/Service/FacebookApi.php
@@ -3,4 +3,49 @@ namespace AppBundle\Service;
 
 class FacebookApi {
     
+    private $fb;
+    private $config;
+    
+    public function __construct(array $facebookConfig) {
+        $this->config = $facebookConfig;
+        
+        $this->fb = new \Facebook\Facebook([
+            "app_id" => $this->config["app_id"],
+            "app_secret" => $this->config["app_secret"],
+            "default_graph_version" => "v2.12",
+        ]);
+    }
+
+    public function getLikes() {
+        try {
+            // Returns a `Facebook\FacebookResponse` object
+            $response = $this->fb->get('/cpnv.ch?fields=fan_count', $$this->configthis->config["access_token"]);
+
+        } catch(Facebook\Exceptions\FacebookResponseException $e) {
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch(Facebook\Exceptions\FacebookSDKException $e) {
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+
+        return $response->getGraphNode()["fan_count"];
+    }
+
+    public function getPosts(int $nbPosts) {
+        try {
+            // Returns a `Facebook\FacebookResponse` object
+            $response = $this->fb->get('/cpnv.ch/feed', $this->config["access_token"]);
+
+        } catch(Facebook\Exceptions\FacebookResponseException $e) {
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch(Facebook\Exceptions\FacebookSDKException $e) {
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+        $posts = $response->getGraphEdge()->asArray();
+        return array_splice($posts, 0, $nbPosts);
+    }
+
 }
