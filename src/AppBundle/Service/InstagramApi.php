47be65ca94b81249ea8ed9ceb9a63106a3ba27e2
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-03-16 08:42:26 +0100

    implement InstagramApi  service

--- src/AppBundle/Service/InstagramApi.php
+++ src/AppBundle/Service/InstagramApi.php
@@ -3,4 +3,26 @@ namespace AppBundle\Service;
 
 class InstagramApi {
     
+    private $ig;
+    private $account = "cpnv.ch";
+    private $config;
+
+    function __construct(array $instagramConfig) {
+        // save instagram config
+        $this->config = $instagramConfig;
+        // create new Guzzle client
+        $client = new \GuzzleHttp\Client();
+        // get data
+        $results = $client->request('GET', "{$this->config['api_url']}{$this->account}/{$this->config['api_param']}");
+        $this->ig = json_decode($results->getBody()->getContents())->graphql;
+    }
+
+    function getFollowerCount() {
+        return $this->ig->user->edge_followed_by->count;
+    }
+
+    function getPosts(int $nbPosts) {
+        $posts = $this->ig->user->edge_owner_to_timeline_media->edges;
+        return array_splice($posts, 0, $nbPosts);
+    }
 }
