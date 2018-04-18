8d223e865886df5d33244cca11ed0f7d4f7c5383
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-04-18 09:32:23 +0200

    Quick fix for instagram api
    The app used a sneaky backdoor on instagram so it could get user info without asking for permission. But it got shutdown so a quickfix was done so the app could sitll work even though nothing is returned for instagram

--- src/AppBundle/Service/InstagramApi.php
+++ src/AppBundle/Service/InstagramApi.php
@@ -2,7 +2,7 @@
 namespace AppBundle\Service;
 
 class InstagramApi {
-    
+
     private $ig;
     private $account = "cpnv.ch";
     private $config;
@@ -13,8 +13,21 @@ class InstagramApi {
         // create new Guzzle client
         $client = new \GuzzleHttp\Client();
         // get data
-        $results = $client->request('GET', "{$this->config['api_url']}{$this->account}/{$this->config['api_param']}");
-        $this->ig = json_decode($results->getBody()->getContents())->graphql;
+        try {
+            $results = $client->request('GET', "{$this->config['api_url']}{$this->account}/{$this->config['api_param']}");
+            $this->ig = json_decode($results->getBody()->getContents())->graphql;
+        } catch (\Exception $e) {
+            // The "hack" used to access instagram information was shutdown. So a quick workaround was done so app would still work
+            $this->ig = new \stdClass();
+            $this->ig->user = new \stdClass();
+            $this->ig->user->edge_followed_by = new \stdClass();
+            $this->ig->user->edge_owner_to_timeline_media = new \stdClass();
+
+            $this->ig->user->edge_followed_by->count = 0;
+            $this->ig->user->edge_owner_to_timeline_media->edges = [];
+            $this->ig->user->full_name = $this->account;
+            $this->ig->user->profile_pic_url_hd = "https://scontent-frt3-1.cdninstagram.com/vp/d062162bd4a50683822f74e1b763cea0/5B543695/t51.2885-19/14592180_1850795251810375_6654409392533798912_a.jpg";
+        }
     }
 
     function getFollowerCount() {
